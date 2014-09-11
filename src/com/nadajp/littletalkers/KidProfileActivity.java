2fa767e72fbb7d29c4ae345782d03f1390c2a7c7
Nada J. Pivcevic <nada.jaksic@gmail.com>
2014-09-10 21:50:32 -0400

    make KidProfileActivity extend Activity
    - KidProfileActivity no longer extends BaseActivity

--- src/com/nadajp/littletalkers/KidProfileActivity.java
+++ src/com/nadajp/littletalkers/KidProfileActivity.java
@@ -3,13 +3,14 @@ package com.nadajp.littletalkers;
 import com.nadajp.littletalkers.utils.Utils;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.MenuItem;
 
-public class KidProfileActivity extends BaseActivity
+public class KidProfileActivity extends Activity
 {
 
    @Override
@@ -27,13 +28,14 @@ public class KidProfileActivity extends BaseActivity
       Utils.setColor(actionBar, Utils.COLOR_RED, this);
       actionBar.setDisplayShowHomeEnabled(true);
       actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-      actionBar.setDisplayShowCustomEnabled(false);      
       actionBar.setTitle(R.string.title_activity_kid_profile);
    }
 
    @Override
-   public boolean onOptionsItemSelected(MenuItem item) {
-       switch (item.getItemId()) {
+   public boolean onOptionsItemSelected(MenuItem item) 
+   {
+       switch (item.getItemId()) 
+       {
        // Respond to the action bar's Up/Home button
        case android.R.id.home:
            if (this.getIntent().hasExtra("ManageKidsView"))
@@ -47,9 +49,16 @@ public class KidProfileActivity extends BaseActivity
               // navigate up to the logical parent activity.
               NavUtils.navigateUpTo(this, intent);
               //startActivity(intent);
-           }
-           
+           }          
            return true;
+       case R.id.action_export:
+          Intent backup_intent = new Intent(this, DataExportActivity.class);
+          startActivity(backup_intent);
+          return true;
+       case R.id.action_manage_kids:
+          Intent manage_intent = new Intent(this, ManageKidsActivity.class);
+          startActivity(manage_intent);
+          return true;
        }
        return super.onOptionsItemSelected(item);
    }
